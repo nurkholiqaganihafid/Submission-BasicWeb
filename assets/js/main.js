17b7de2f973da86f0fb4199b81afa90c3a29631e
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2022-09-20 01:47:05 +0700

    implement ship content popup

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+function togglePopupMerry() {
+    document.getElementById('popup-1').classList.toggle('active')
+}
+
+function togglePopupSunny() {
+    document.getElementById('popup-2').classList.toggle('active')
+}
+
+// document.addEventListener('keydown', function (event) {
+//     if (event.key === "Escape") {
+//         document.getElementById("popup-1").classList.toggle("active");
+//     }
+// });
