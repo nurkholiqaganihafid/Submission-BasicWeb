66e0a58288de0a530e28a6182e3bd1f8f9a0d9d3
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2022-09-20 17:15:12 +0700

    implement gallery content popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,62 @@ function togglePopupSunny() {
     document.getElementById('popup-2').classList.toggle('active')
 }
 
+function togglePopupGaleri1() {
+    document.getElementById('popup-galeri1').classList.toggle('active')
+}
+
+function togglePopupGaleri2() {
+    document.getElementById('popup-galeri2').classList.toggle('active')
+}
+
+function togglePopupGaleri3() {
+    document.getElementById('popup-galeri3').classList.toggle('active')
+}
+
+function togglePopupGaleri4() {
+    document.getElementById('popup-galeri4').classList.toggle('active')
+}
+
+function togglePopupGaleri5() {
+    document.getElementById('popup-galeri5').classList.toggle('active')
+}
+
+function togglePopupGaleri6() {
+    document.getElementById('popup-galeri6').classList.toggle('active')
+}
+
+function togglePopupGaleri7() {
+    document.getElementById('popup-galeri7').classList.toggle('active')
+}
+
+function togglePopupGaleri8() {
+    document.getElementById('popup-galeri8').classList.toggle('active')
+}
+
+function togglePopupGaleri9() {
+    document.getElementById('popup-galeri9').classList.toggle('active')
+}
+
+function togglePopupGaleri10() {
+    document.getElementById('popup-galeri10').classList.toggle('active')
+}
+
+function togglePopupGaleri11() {
+    document.getElementById('popup-galeri11').classList.toggle('active')
+}
+
+function togglePopupGaleri12() {
+    document.getElementById('popup-galeri12').classList.toggle('active')
+}
+
+function togglePopupGaleri13() {
+    document.getElementById('popup-galeri13').classList.toggle('active')
+}
+
+function togglePopupGaleri14() {
+    document.getElementById('popup-galeri14').classList.toggle('active')
+}
+
 // document.addEventListener('keydown', function (event) {
 //     if (event.key === "Escape") {
 //         document.getElementById("popup-1").classList.toggle("active");
